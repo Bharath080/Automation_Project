319e64157e75333055d61b36d5829c4dd3d4202c
root <root@ip-172-31-15-7.ec2.internal>
2023-03-01 17:41:32 +0000

    This is new Test file

--- automation.sh
+++ automation.sh
@@ -1,3 +1,30 @@
+sudo apt update -y
+
+ps cax | grep httpd
+if [ $? -eq 0 ]; then
+ echo "Process is running."
+else
+ systemctl start apache2.service
+
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+then
+echo "$service is running!!!"
+else
+/etc/init.d/$service start
+fi
+
+SERVICE_NAME="httpd"
+
+if ! systemctl is-enabled $SERVICE_NAME >/dev/null 2>&1; then
+    # If the service is not enabled, enable it
+    systemctl enable $SERVICE_NAME
+fi
+
+if ! systemctl is-active $SERVICE_NAME >/dev/null 2>&1; then
+    # If the service is not running, start it
+    systemctl start $SERVICE_NAME
+fi
+
 cd /var/log/apache2
 
 timestamp=$(date '+%d%m%Y-%H%M%S')
