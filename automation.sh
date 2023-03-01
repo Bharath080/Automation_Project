042b8ac278b6114b26b5cd35dca0895743d1be53
root <root@ip-172-31-15-7.ec2.internal>
2023-03-01 17:52:23 +0000

    This is a new HTTP Test file

--- automation.sh
+++ automation.sh
@@ -1,5 +1,47 @@
-cd /var/log/apache2
+sudo apt update -y
 
+ps cax | grep httpd
+if [ $? -eq 0 ]; then
+ echo "Process is running."
+else
+ systemctl start apache2.service
+
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+then
+echo "$service is running!!!"
+else
+/etc/init.d/$service start
+fi
+
+SERVICE_NAME="httpd"
+
+if ! systemctl is-enabled $SERVICE_NAME >/dev/null 2>&1; then
+   
+    systemctl enable $SERVICE_NAME
+fi
+
+if ! systemctl is-active $SERVICE_NAME >/dev/null 2>&1; then
+   
+    systemctl start $SERVICE_NAME
+fi
+
+if [ ! -f /var/www/html/inventory.html ]; then
+  
+    cat > /var/www/html/inventory.html <<EOF
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Archived Logs Inventory</title>
+</head>
+<body>
+	<h1>Archived Logs Inventory</h1>
+	<p>Metadata of the archived logs will be listed here.</p>
+</body>
+</html>
+EOF
+fi
+
+cd /var/log/apache2
 timestamp=$(date '+%d%m%Y-%H%M%S')
 
 sudo tar -cvzf /tmp/bharath-httpd-logs-$timestamp.tar.gz access.log error.log other_vhosts_access.log
